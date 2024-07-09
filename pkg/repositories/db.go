1f4b8be100082dac975bb769f220196236387ebb
linshan <1099013371@qq.com>
2024-07-09 10:18:57 +0800

    fix: 新增仓库方法

--- pkg/repositories/db.go
+++ pkg/repositories/db.go
@@ -9,11 +9,16 @@ const (
 var _ DbRepo = (*dbRepo)(nil)
 
 type DbRepo interface {
+	Count() int
 	DB(name string) db.Db
 }
 
 type dbRepo struct {
 	resource map[string]db.Db
+}
+
+func (repo *dbRepo) Count() int {
+	return len(repo.resource)
 }
 
 func (repo *dbRepo) DB(name string) db.Db {
--- pkg/repositories/redis.go
+++ pkg/repositories/redis.go
@@ -9,11 +9,16 @@ const (
 var _ RedisRepo = (*redisRepo)(nil)
 
 type RedisRepo interface {
+	Count() int
 	Redis(name string) cache.Redis
 }
 
 type redisRepo struct {
 	resource map[string]cache.Redis
+}
+
+func (repo *redisRepo) Count() int {
+	return len(repo.resource)
 }
 
 func (repo *redisRepo) Redis(name string) cache.Redis {
